15ce0b0cc0903f22e583f1b251981376268f844d
puxiao <yangpuxiao@gmail.com>
2025-06-16 16:46:34 +0800

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,4 @@
 export const RATIO_Inches_EMUs = 914400 // 1英寸 = 914400EMUs
 export const RATIO_Inches_Points = 72 // 1英寸 = 72pt
 export const RATIO_EMUs_Points = RATIO_Inches_Points / RATIO_Inches_EMUs // 1EMUs = (72 / 914400)pt
+export const BASE64_ERROR_IMG = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=' // 缺省图片(1x1px)
